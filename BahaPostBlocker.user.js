15546e0d14a137f8b5697eb132835723bbc21c6c
lllidontknowl <wuken840325@gmail.com>
2017-07-01 17:14:09 +0800

    Update BahaPostBlocker.user.js

--- BahaPostBlocker.user.js
+++ BahaPostBlocker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BahaPostBlocker
-// @version      1.3
+// @version      1.31
 // @description  把含有關鍵字的文章和黑名單從文中列表中濾掉
 // @author       wuken0325
 // @include      *forum.gamer.com.tw/*
@@ -8,8 +8,8 @@
 
 
     
-var Block_str=["康妮","寶拉","蔡英文","馬英九","聖結","聖粉","結石","放火","賴界","小幻","康乙己"]; //關鍵字
-var Block_auther=["daved070708"]; //黑名單
+var Block_str=["康妮","寶拉","蔡英文","馬英九","聖結","聖粉","結石","放火","賴界","小幻","康乙己"]; //關鍵字，依所需自行修改
+var Block_auther=["daved070708","bennysit2003"]; //黑名單，依所需自行修改
 var Delete_cell0=[];
 var Delete_cell1=[];
 var Delete_cell2=[];
@@ -22,7 +22,13 @@ var auther_tmp='';
 var block = false;
 var mtable=document.getElementsByClassName("FM-blist fmb");
 
-document.getElementsByClassName("BH-pagebtnA")[0].innerHTML += "<a onclick=\"Post_insert(mtable,Delete_post)\">復原文章</a>" //加入按鈕
+document.getElementsByClassName("BH-pagebtnA")[0].innerHTML += "<a id=\"rebutton\">復原文章</a>"; //加入按鈕
+var myButton = document.getElementById("rebutton"); //獲取剛加入的按鈕
+myButton.addEventListener("click", function() {  //給按鈕上個監聽
+    Post_insert(mtable,Delete_post);
+});
+
+
 
 for (i=1;i<mtable[0].rows.length;i++){
     block = false;
@@ -40,7 +46,7 @@ for (i=1;i<mtable[0].rows.length;i++){
         }
     }
 
-    if(block==true){
+    if(block===true){
         Delete_cell0[Delete_cell0.length]=mtable[0].rows[i].cells[0].innerHTML; //獲取標題的各種值
         Delete_cell1[Delete_cell1.length]=mtable[0].rows[i].cells[1].innerHTML;
         Delete_cell2[Delete_cell2.length]=mtable[0].rows[i].cells[2].innerHTML;
@@ -51,6 +57,8 @@ for (i=1;i<mtable[0].rows.length;i++){
         i=i-1;
     }
 }
+
+
 
 function Post_insert(mtable,Delete_post){   //復原文章
     var classname=["","FM-blist2","FM-blist3","FM-blist4","FM-blist5","FM-blist6"]; //給td的classname
