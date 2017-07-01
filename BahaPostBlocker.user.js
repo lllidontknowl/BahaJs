7eb8922933b1619ae62fbace4b5103a949f59eab
lllidontknowl <wuken840325@gmail.com>
2017-07-01 16:22:46 +0800

    Update BahaPostBlocker.user.js

--- BahaPostBlocker.user.js
+++ BahaPostBlocker.user.js
@@ -10,7 +10,13 @@
     
 var Block_str=["康妮","寶拉","蔡英文","馬英九","聖結","聖粉","結石","放火","賴界","小幻","康乙己"]; //關鍵字
 var Block_auther=["ss54564152","ru3715","chocolatebox","bennysit2003","daved070708"]; //黑名單
-var Delete_post=[];
+var Delete_cell0=[];
+var Delete_cell1=[];
+var Delete_cell2=[];
+var Delete_cell3=[];
+var Delete_cell4=[];
+var Delete_cell5=[];
+var Delete_post=[Delete_cell0,Delete_cell1,Delete_cell2,Delete_cell3,Delete_cell4,Delete_cell5];
 var title_tmp='';
 var auther_tmp='';
 var block = false;
@@ -35,7 +41,12 @@ for (i=1;i<mtable[0].rows.length;i++){
     }
 
     if(block==true){
-        Delete_post[Delete_post.length]=mtable[0].rows[i].innerHTML;
+        Delete_cell0[Delete_cell0.length]=mtable[0].rows[i].cells[0].innerHTML;
+        Delete_cell1[Delete_cell1.length]=mtable[0].rows[i].cells[1].innerHTML;
+        Delete_cell2[Delete_cell2.length]=mtable[0].rows[i].cells[2].innerHTML;
+        Delete_cell3[Delete_cell3.length]=mtable[0].rows[i].cells[3].innerHTML;
+        Delete_cell4[Delete_cell4.length]=mtable[0].rows[i].cells[4].innerHTML;
+        Delete_cell5[Delete_cell5.length]=mtable[0].rows[i].cells[5].innerHTML;
         mtable[0].deleteRow(i);
         i=i-1;
     }
@@ -44,8 +55,10 @@ for (i=1;i<mtable[0].rows.length;i++){
 function Post_insert(mtable,Delete_post){
     for(i=0;i<Delete_post.length;i++){
         var New_post = mtable[0].insertRow(1);
-        var Post_inside = New_post.insertCell(0);
-        Post_inside.innerHTML = Delete_post[i];
+        for(j=0;j<Delete_post[i].length+1;j++){
+            var Post_inside_tmp = New_post.insertCell(-1);
+            Post_inside_tmp.innerHTML = Delete_post[j][i];
+        }
     }
 }
     
