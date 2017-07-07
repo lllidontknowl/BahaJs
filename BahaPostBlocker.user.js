b41f25f34338e82af953ce165e8653a2ac0f7e01
lllidontknowl <wuken840325@gmail.com>
2017-07-07 22:19:21 +0800

    Update BahaPostBlocker.user.js

--- BahaPostBlocker.user.js
+++ BahaPostBlocker.user.js
@@ -1,10 +1,8 @@
 // ==UserScript==
 // @name         BahaPostBlocker
-// @version      1.4
+// @version      1.32
 // @description  把含有關鍵字的文章和黑名單從文中列表中濾掉
 // @author       wuken0325
-// @homepageURL	 https://github.com/lllidontknowl/BahaJs
-// @updateURL	 https://github.com/lllidontknowl/BahaJs/raw/master/BahaPostBlocker.user.js
 // @include      *forum.gamer.com.tw/*
 // ==/UserScript==
 
